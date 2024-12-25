cf1405f5f6b489a1405bf43095841db5eb753675
devjuanmarcos <jncontatojn@gmail.com>
2024-12-25 14:51:07 -0300

    feat: endpoints de investimentos

--- src/controllers/transacoesController.ts
+++ src/controllers/transacoesController.ts
@@ -82,25 +82,6 @@ export const listarTransacoes = async (req: Request, res: Response) => {
 export const removerTransacao = async (req: Request, res: Response) => {
   const { id } = req.params;
 
-  // Obtenha o access token do cabeçalho da requisição
-  const accessToken = req.headers["supabase.token"] as string;
-  const refreshToken = req.headers["supabase.refresh_token"] as string;
-
-  if (!accessToken || !refreshToken) {
-    return res.status(401).json({ error: "Token não fornecido" });
-  }
-
-  // Defina a sessão com os tokens
-  const { error: sessionError } = await supabase.auth.setSession({
-    access_token: accessToken,
-    refresh_token: refreshToken,
-  });
-
-  if (sessionError) {
-    return res.status(401).json({ error: "Não autorizado" });
-  }
-
-  // Remova a transação
   const { data, error } = await supabase
     .from("transacoes")
     .delete()
@@ -167,4 +148,59 @@ export const listarGastosGanhosFixos = async (req: Request, res: Response) => {
       error: error instanceof Error ? error.message : "Erro desconhecido",
     });
   }
+};
+
+export const investimentos = async (req: Request, res: Response) => {
+  // const { mes_ano } = req.query;
+
+  const { data, error } = await supabase
+    .from("financas_investimento")
+    .select("*");
+  // .eq("mes_ano", mes_ano);
+
+  if (error) return res.status(400).json({ error: error.message });
+  return res.status(200).json(data);
+};
+
+export const criarInvestimento = async (req: Request, res: Response) => {
+  const { quantidade, categoria_id } = req.body;
+
+  const { data, error } = await supabase.from("financas_investimento").insert({
+    quantidade,
+    categoria_id,
+  });
+
+  if (error) return res.status(400).json({ error: error.message });
+  return res.status(201).json(data);
+};
+
+export const removerInvestimento = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const { data, error } = await supabase
+    .from("financas_investimento")
+    .delete()
+    .eq("id", id);
+
+  if (error) return res.status(400).json({ error: error.message });
+  return res.status(200).json(data);
+};
+
+export const atualizarInvestimento = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { quantidade, categoria_id } = req.body;
+
+  const finalData = {
+    quantidade: quantidade,
+    categoria_id: categoria_id,
+  };
+
+  const { data, error } = await supabase
+    .from("financas_investimento")
+    .update([finalData])
+    .eq("id", id)
+    .select();
+
+  if (error) return res.status(400).json({ error: error.message });
+  return res.status(200).json(data);
 };
--- src/routes/transacoes.ts
+++ src/routes/transacoes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import {
+  atualizarInvestimento,
+  criarInvestimento,
   criarTransacao,
+  investimentos,
   listarGastosGanhosFixos,
   listarSubCategoriasPorId,
   listarTransacoes,
+  removerInvestimento,
   removerTransacao,
   resumoMensal,
 } from "../controllers/transacoesController";
@@ -16,16 +20,30 @@ const asyncHandler = (fn: Function) => (req: any, res: any, next: any) => {
   Promise.resolve(fn(req, res, next)).catch(next);
 };
 
+router.post("/login", asyncHandler(loginUser));
+router.post("/login-with-id", asyncHandler(LoginUserWithToken));
+
 router.post("/transacoes", verifyToken, asyncHandler(criarTransacao));
 router.get("/transacoes", asyncHandler(listarTransacoes));
-router.delete("/transacoes/:id", asyncHandler(removerTransacao));
+router.delete("/transacoes/:id", verifyToken, asyncHandler(removerTransacao));
+router.get("/transacoes/fixos", asyncHandler(listarGastosGanhosFixos));
 router.get("/resumo-mensal", asyncHandler(resumoMensal));
 router.get(
   "/subcategorias/:categoria_id",
   asyncHandler(listarSubCategoriasPorId)
 );
-router.get("/transacoes/fixos", asyncHandler(listarGastosGanhosFixos));
-router.post("/login", asyncHandler(loginUser));
-router.post("/login-with-id", asyncHandler(LoginUserWithToken));
+
+router.get("/investimentos", asyncHandler(investimentos));
+router.post("/investimentos", verifyToken, asyncHandler(criarInvestimento));
+router.delete(
+  "/investimentos/:id",
+  verifyToken,
+  asyncHandler(removerInvestimento)
+);
+router.put(
+  "/investimentos/:id",
+  verifyToken,
+  asyncHandler(atualizarInvestimento)
+);
 
 export default router;
