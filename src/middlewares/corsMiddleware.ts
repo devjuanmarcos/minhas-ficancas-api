ea158e3a140fdb5b7aebf29d94e52ba857e097b9
devjuanmarcos <jncontatojn@gmail.com>
2024-12-25 11:37:20 -0300

    fix: teste cors

--- src/middlewares/corsMiddleware.ts
+++ src/middlewares/corsMiddleware.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 export const corsMiddleware = cors({
   origin: ["*"],
   methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "supabase.token", "supabase.refresh_token"],
+  allowedHeaders: ["*"],
   preflightContinue: false, // Não continuar o processamento da pre-flight se for um sucesso
   optionsSuccessStatus: 204, // Responde com 204 em vez de 200 em pre-flight, se sucesso
   credentials: true, // Permite o envio de cookies e autenticação
