3342457be846540ac2c0f5e2191b1f16c8c92b11
devjuanmarcos <jncontatojn@gmail.com>
2024-12-25 10:18:30 -0300

    fix: teste token

--- src/controllers/transacoesController.ts
+++ src/controllers/transacoesController.ts
@@ -25,6 +25,10 @@ export const criarTransacao = async (req: Request, res: Response) => {
   console.log("Access Token:", accessToken);
   console.log("Refresh Token:", refreshToken);
 
+  if (!accessToken || !refreshToken) {
+    return res.status(401).json({ error: "Token não fornecido" });
+  }
+
   const { error: sessionError } = await supabase.auth.setSession({
     access_token: accessToken,
     refresh_token: refreshToken,
