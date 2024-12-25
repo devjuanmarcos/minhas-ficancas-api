b83387f8c641e7ac4e8858d79d1c3ee02c278579
devjuanmarcos <jncontatojn@gmail.com>
2024-12-25 10:57:18 -0300

    fix: teste cors

--- src/middlewares/corsMiddleware.ts
+++ src/middlewares/corsMiddleware.ts
@@ -1,13 +1,9 @@
 import cors from "cors";
 
 export const corsMiddleware = cors({
-  origin: ["https://minhas-ficancas-api.vercel.app"], // URL do frontend
-  methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
-  allowedHeaders: [
-    "Content-Type",
-    "supabase.token",
-    "supabase.refresh_token", // Headers personalizados
-  ],
+  origin: ["*"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "supabase.token", "supabase.refresh_token"],
   preflightContinue: false, // Não continuar o processamento da pre-flight se for um sucesso
   optionsSuccessStatus: 204, // Responde com 204 em vez de 200 em pre-flight, se sucesso
   credentials: true, // Permite o envio de cookies e autenticação
